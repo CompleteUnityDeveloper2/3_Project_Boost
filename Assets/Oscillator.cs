dc66b436489dabc1cd4f69214866b7423072e429
Ben Tristem <github@bentristem.com>
2019-07-14 23:05:13 +0100

    Refactor

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -7,6 +7,7 @@ public class Oscillator : MonoBehaviour {
 
     [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
     [SerializeField] float period = 2f;
+    [SerializeField] float movementOffset = 0.5f;
 
     float movementFactor; // 0 for not moved, 1 for fully moved.
     Vector3 startingPos;
@@ -24,7 +25,7 @@ public class Oscillator : MonoBehaviour {
         const float tau = Mathf.PI * 2f; // about 6.28
         float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
 
-        movementFactor = rawSinWave / 2f + 0.5f;
+        movementFactor = rawSinWave / 2f + movementOffset;
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
 	}
